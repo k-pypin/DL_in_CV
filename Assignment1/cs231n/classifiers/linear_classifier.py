d65b0eb4bc5fd6a20f7342ac1e55572f632204f8
MrTrojan52 <kirill_131097@mail.ru>
2019-12-18 22:22:46 +0300

    Softmax: Initial commit

--- Assignment1/cs231n/classifiers/linear_classifier.py
+++ Assignment1/cs231n/classifiers/linear_classifier.py
@@ -51,7 +51,9 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      pass
+      batch_ids = np.random.choice(num_train, batch_size, replace=False)
+      X_batch = X[batch_ids]
+      y_batch = y[batch_ids]
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -65,7 +67,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W -= learning_rate * grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -94,7 +96,7 @@ class LinearClassifier(object):
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    y_pred = np.argmax(X.dot(self.W), axis=1)
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
--- Assignment1/cs231n/classifiers/softmax.py
+++ Assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,25 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  S = X.dot(W)
+  train_size = X.shape[0]
+  classes_cnt = W.shape[1]
+
+  for i in range(train_size):
+    f = S[i] - np.max(S[i])
+    sm = np.exp(f) / np.sum(np.exp(f))
+    loss += -np.log(sm[y[i]])
+
+    for j in range(classes_cnt):
+      dW[:, j] += X[i] * sm[j]
+
+    dW[:, y[i]] -= X[i]
+
+  loss /= train_size
+  dW /= train_size
+
+  loss += reg * np.sum(W * W)
+  dW += reg * 2 * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +71,23 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  S = X.dot(W)
+  train_size = X.shape[0]
+  S -= np.max(S, axis=1, keepdims=True)
+
+  sum_exp_S = np.exp(S).sum(axis=1, keepdims=True)
+  sm_matrix = np.exp(S) / sum_exp_S
+
+  loss = np.sum(-np.log(sm_matrix[np.arange(train_size), y]))
+
+  sm_matrix[np.arange(train_size), y] -= 1
+  dW = X.T.dot(sm_matrix)
+
+  loss /= train_size
+  dW /= train_size
+
+  loss += reg * np.sum(W * W)
+  dW += reg * 2 * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
